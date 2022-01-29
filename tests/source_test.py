fe6e791d0e9e7f40834266a68332c7bafc3efeca
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-01-29 19:54:55 +0300

    Createda a test for the Source class

---
+++ tests/source_test.py
@@ -0,0 +1,19 @@
+import unittest
+from app.models import Source
+
+class SourceTest(unittest.TestCase):
+    '''
+    Test class to test the behaviour of the Source class
+    '''
+    def setUp(self):
+        '''
+        Set up method that will run before every test
+        '''
+        self.new_source = Source("Cnn", "https://cnn.com", "Top news worldwide", 1)
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_source, Source))    
+
+
+if __name__ == '__main__':
+    unittest.main()
