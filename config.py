8a533ba30258b75b988f8739c9b7760ce436595b
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-01-29 20:11:55 +0300

    Created configurations for the project

---
+++ config.py
@@ -0,0 +1,28 @@
+from distutils.command import config
+from distutils.debug import DEBUG
+from instance.config import NEWS_API_KEY
+import os
+
+class Config:
+    '''
+    General configuration parent class
+    '''
+    NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/'
+
+class ProdConfig(Config):
+    '''
+    Production Configuration class
+    '''
+    pass
+
+class DevConfig(Config):
+    '''
+    Development configuration child class 
+    '''
+    DEBUG = True
+
+config_options = {
+    'development' : DevConfig,
+    'production' : ProdConfig
+}
