b127eadab61756b2d27bf353ed0a838ec4cd62dd
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-01-29 19:41:57 +0300

    Created a class for source

---
+++ app/models.py
@@ -0,0 +1,9 @@
+class Source:
+    '''
+    Source class to define source objects
+    '''
+    def __init__(self, name, url, description, id):
+        self.name = name
+        self.id = id
+        self.url = url
+        self.description = description
