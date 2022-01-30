e866b4ce92cef759b84fc635b99deb2439442b9a
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-01-30 15:05:49 +0300

    Created a test for the News class

---
+++ tests/news_test.py
@@ -0,0 +1,19 @@
+import unittest
+from app.models import News
+
+class NewsTest(unittest.TestCase):
+    '''
+    Test class to test the behaviour of the Source class
+    '''
+    def setUp(self):
+        '''
+        Set up method that will run before every test
+        '''
+        self.new_news_article = News("Ruweydha","Hackathon has began", "Hackathon in kenta has began" "https://cnn.com", "imageurl", "21st January, 2022")
+
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_news_article, News))    
+
+
+if __name__ == '__main__':
+    unittest.main()
