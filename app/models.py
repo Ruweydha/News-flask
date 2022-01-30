06797071fe33787318fa65afb50b61557deed4ff
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-01-30 15:06:51 +0300

    Created a news class

--- app/models.py
+++ app/models.py
@@ -7,3 +7,12 @@ class Source:
         self.id = id
         self.url = url
         self.description = description
+
+class News:
+    def __init__(self, author, content, title, url, urlToImage, publishedAt ):
+        self.author = author
+        self.description = content 
+        self.title = title
+        self.url = url
+        self.urlToImage = urlToImage
+        self.publishedAt = publishedAt
