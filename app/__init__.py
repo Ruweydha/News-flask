9599249d6924dcdade70c3c72f07b6bc9e5f5db0
Ruweydha Abdinoor <ruweydhaabdinoor@gmail.com>
2022-01-31 15:25:07 +0300

    Removed bootstrap initialization

--- app/__init__.py
+++ app/__init__.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask_bootstrap import Bootstrap
 from config import config_options
 
-bootstrap = Bootstrap()
+
 
 def create_app(config_name):
     app = Flask(__name__)
@@ -13,7 +13,7 @@ def create_app(config_name):
     app.config.from_object(config_options[config_name])
 
     #Initializing flask extensions
-    bootstrap.init_app(app)
+  
 
     #Registering the blueprint
 
